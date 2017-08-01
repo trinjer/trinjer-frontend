54be423e05ba49f1452c96baaa7be35f80ebca98
Boyko Vitaliy <boyko330@gmail.com>
2017-08-01 22:13:13 +0300

    Added registration component

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -4,6 +4,7 @@ import {UserStore} from './shared/services/user.store';
 import {AngularMaterialModule} from '../shared/angular-material.module';
 import {LoginComponent} from './authentication/login/login.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { RegistrationComponent } from './authentication/registration/registration.component';
 
 @NgModule({
 	imports: [
@@ -15,7 +16,7 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 	providers: [
 		UserStore
 	],
-	declarations: [LoginComponent]
+	declarations: [LoginComponent, RegistrationComponent]
 })
 export class UserModule {
 }
